3e22d41222c23333fcc0f4bb7325ebe4fffb7e19
taxijjang <gw9122@Naver.com>
2019-04-15 20:27:39 +0900

    #4 solve sw_1218

--- d4/sw_1218/taxijjang/sw_1218.cpp
+++ d4/sw_1218/taxijjang/sw_1218.cpp
@@ -4,64 +4,48 @@
 
 using namespace std;
 
+stack<char> stk;
+
 int main(void) {
 	for (int t = 1; t <= 10; t++) {
 		int N;
 		scanf("%d", &N);
 
-		stack<char> s1;// (
-		stack<char> s2;// [
-		stack<char> s3;// {
-		stack<char> s4;// <
+		string s;
+		cin >> s;
 
-		int judge = 1;
+		int s_size = s.size();
 
-		string brk;
-		cin >> brk;
+		int result = 1;
 
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < s_size; i++) {
+			if (s[i] == '(' || s[i] == '[' || s[i] == '{' || s[i] == '<') {
+				stk.push(s[i]);
+			}
+			else if (s[i] == ')' || s[i] == ']' || s[i] == '}' || s[i] == '>') {
+				char top = stk.top();
 
-			if (brk[i] == '(') {
-				s1.push(brk[i]);
+				if (s[i] == ')') {
+					if (top == s[i] - 1) {
+						stk.pop();
+					}
+					else {
+						result = 0;
+						break;
+					}
+				}
+				else if (s[i] == ']' || s[i] == '}' || s[i] == '>') {
+					if (top == s[i] - 2) {
+						stk.pop();
+					}
+					else {
+						result = 0;
+						break;
+					}
+				}
 			}
-			else if (brk[i] == '[') {
-				s2.push(brk[i]);
-			}
-			else if (brk[i] == '{') {
-				s3.push(brk[i]);
-			}
-			else if (brk[i] == '<') {
-				s4.push(brk[i]);
-			}
-			
-			else if (brk[i] == ')') {
-				if (s1.empty())
-					judge = 0;
-				else
-					s1.pop();
-			}
-			else if (brk[i] == ']') {
-				if (s2.empty())
-					judge = 0;
-				else
-					s2.pop();
-			}
-			else if (brk[i] == '}') {
-				if (s3.empty())
-					judge = 0;
-				else
-					s3.pop();
-			}
-			else if (brk[i] == '>') {
-				if (s4.empty())
-					judge = 0;
-				else
-					s4.pop();
-			}
-
-			if (judge == 0)
-				break;
 		}
-		printf("#%d %d\n", t, judge);
+
+		printf("#%d %d\n", t, result);
 	}
 }
