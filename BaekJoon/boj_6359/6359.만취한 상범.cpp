d19e0507578da0155bf34eebdf37e6aa3927d65f
unknown <t2korea@naver.com>
2019-08-01 01:17:58 +0900

    solve #17 boj_6359

---
+++ BaekJoon/boj_6359/6359.만취한 상범.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int tc, t;
+	cin >> t;
+
+	for (int tc = 0; tc < t; tc++) {
+
+		int n;
+		cin >> n;
+
+		int open = sqrt(n);
+		cout << open << endl;
+	}
+	return 0;
+}
