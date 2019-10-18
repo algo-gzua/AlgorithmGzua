3468549f4f39e47b090f817ef0761079d14428f4
taxijjang <gw9122@naver.com>
2019-10-19 02:30:09 +0900

    solve #26 boj_2908

---
+++ BaekJoon/boj_2908/taxijjang/boj_2908.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main(void) {
+	string a, b; cin >> a >> b;
+
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+
+	cout << (a < b ? b : a);
+}
